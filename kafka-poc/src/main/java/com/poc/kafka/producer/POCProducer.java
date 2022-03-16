4a729f23c315bb679a0ff698bae7e74b7545b75c
dipanjal <dipanjalmaitra@gmail.com>
2022-03-16 14:35:59 +0600

    Publish with Result Callback added

--- kafka-poc/src/main/java/com/poc/kafka/producer/POCProducer.java
+++ kafka-poc/src/main/java/com/poc/kafka/producer/POCProducer.java
@@ -6,9 +6,13 @@ import com.poc.kafka.config.KafkaConfig;
 import com.poc.kafka.model.message.KafkaMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.awt.geom.RectangularShape;
 import java.util.Date;
 import java.util.UUID;
 
@@ -23,7 +27,23 @@ public class POCProducer<T extends KafkaMessage> {
         message.setMessageId(UUID.randomUUID().toString());
         message.setDateTime(new Date());
         log.info("Producing {}", message);
-        kafkaTemplate.send(KafkaConfig.POC_TOPIC, "payment-key", message);
+        kafkaTemplate.send(KafkaConfig.POC_TOPIC, "payment-key", message)
+                .addCallback(new ListenableFutureCallback<>() {
+                    @Override
+                    public void onFailure(Throwable ex) {
+                        log.error(ex.getMessage(), ex);
+                    }
+
+                    @Override
+                    public void onSuccess(SendResult<String, T> result) {
+                        RecordMetadata metadata = result.getRecordMetadata();
+                        log.info(
+                                "{} \n Message Delivered to {} topic on partition {} and offset {}",
+                                result.getProducerRecord().value(),
+                                metadata.topic(), metadata.partition(), metadata.offset()
+                        );
+                    }
+                });
         return message.getMessageId();
     }
 }
