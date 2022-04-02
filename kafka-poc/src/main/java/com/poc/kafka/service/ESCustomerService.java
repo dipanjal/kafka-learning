35757ed8892733c2ba6fa0c10f79f437beaaec81
dipanjal <dipanjalmaitra@gmail.com>
2022-04-02 19:27:08 +0600

    es customer service code refactoring

--- kafka-poc/src/main/java/com/poc/kafka/service/ESCustomerService.java
+++ kafka-poc/src/main/java/com/poc/kafka/service/ESCustomerService.java
@@ -4,22 +4,20 @@ import com.poc.kafka.config.ElasticSearchConfig;
 import com.poc.kafka.model.Customer;
 import com.poc.kafka.repository.es.ESCustomerRepository;
 import com.poc.kafka.service.mapper.CustomerMapper;
+import com.poc.kafka.service.mapper.SearchResponseMapper;
 import lombok.RequiredArgsConstructor;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -55,48 +53,23 @@ public class ESCustomerService implements SearchService {
 
     @Override
     public List<Customer> customerGlobalSearch(String phrase) throws IOException {
-        SearchRequest request = this.buildSearchRequest(ElasticSearchConfig.INDEX);
-        QueryBuilder queryBuilder = QueryBuilders
-                .boolQuery()
-                .should(QueryBuilders.matchPhrasePrefixQuery("first_name", phrase))
-                .should(QueryBuilders.matchPhrasePrefixQuery("last_name", phrase))
-                .should(QueryBuilders.matchPhrasePrefixQuery("email", phrase))
-                .should(QueryBuilders.matchPhrasePrefixQuery("address", phrase));
-        request.source(
-                new SearchSourceBuilder().query(queryBuilder)
+        QueryBuilder queryBuilder1 = this.buildDynamicQuery(
+                phrase,
+                SearchFieldConstant.getFieldsAsList(SearchFieldConstant.CUSTOMER_GLOBAL_SEARCH_FIELDS)
         );
+
+        SearchRequest request = new SearchRequest(ElasticSearchConfig.INDEX)
+                        .source(new SearchSourceBuilder().query(queryBuilder1));
+
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 
-        ObjectMapper mapper = new ObjectMapper();
-
-        List<Customer> dataMap = Arrays.stream(response.getHits().getHits())
-                .map(SearchHit::getSourceAsMap)
-                .map(customerMap -> mapper.convertValue(customerMap, Customer.class))
-                .collect(Collectors.toList());
-        return dataMap;
+        return SearchResponseMapper.mapSearchResponse(response, Customer.class);
     }
 
-    private SearchRequest buildSearchRequest(final String index) {
-
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.indices(index);
-
-        return searchRequest;
+    private QueryBuilder buildDynamicQuery(String phrase, List<String> fields) {
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        for(String field : fields)
+            queryBuilder.should(QueryBuilders.matchPhrasePrefixQuery(field, phrase));
+        return queryBuilder;
     }
-
-    /*private List<Customer> getSearchResult(SearchResponse response) {
-
-        SearchHit[] searchHit = response.getHits().getHits();
-
-        List<Customer> profileDocuments = new ArrayList<>();
-
-        for (SearchHit hit : searchHit){
-            profileDocuments
-                    .add(objectMapper
-                            .convertValue(hit
-                                    .getSourceAsMap(), ProfileDocument.class));
-        }
-
-        return profileDocuments;
-    }*/
 }
